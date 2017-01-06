893862ce6a651ada4bb5328a8f799229c350d75c
Alexander Aguina <aaguina@gmail.com>
2017-01-06 11:20:35 -0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;    // convention UpperCase var is a constant
+
+// to show Date:
+// new Date().toString()
+
+var middleware = {
+  requireAuthentication: function (req, res, next) {
+    console.log('private route hit!');
+    next();
+  },
+  logger: function (req, res, next) {
+    console.log(new Date().toString() + ' Request: ' + req.method + ' ' + req.originalUrl);
+    next();
+  }
+}
+
+app.use(middleware.logger);
+// app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+  res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+  console.log('Express JS server started at port ' + PORT + '!');
+});
